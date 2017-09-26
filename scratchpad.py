740a6979a946c5ffdc662d04624354bc99780900
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-26 16:01:57 +0530

    Add files via upload
    Uploaded a file with examples of STRIP command

---
+++ scratchpad.py
@@ -0,0 +1,7 @@
+# This line strips out the letter M from MUMBAI
+# NOTE: STRIP removes characters only from the end or beginning of the string
+print("Mumbai".strip('M'))
+
+# It was supposed to strip off the letters DEL in between the word ADELAIDE but instead it
+# stripped off the DE at the end of ADELAIDE
+print("Adelaide".strip('del'))
